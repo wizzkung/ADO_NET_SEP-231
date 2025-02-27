5ceaed47cdd9698635406d27a4f5ae6bb7c18d24
Arthur Pak <sandtorus@gmail.com>
2025-02-27 16:06:30 +0500

    99

---
+++ MySqLite/Orders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqLite
+{
+    public class Orders
+    {
+    }
+
+    public class Order
+    {
+        public int order_id { get; set; }
+        public string order_date { get; set; }
+    }
+
+    public class Product
+    {
+        public int product_id { get; set; }
+        public int quantity { get; set; }
+
+    }
+
+    public class OrderDetails
+    {
+        public int order_id { get; set; }
+        public int product_id { get; set; }
+    }
+    
+
+}
--- MySqLite/Program.cs
+++ MySqLite/Program.cs
@@ -9,7 +9,9 @@ namespace MySqLite
         {
             //MySqLite.GetData3();
             //MySqLite.GetData2();
-            MySqLite.GetData5();
+            // MySqLite.GetData5();
+
+            MySqLite.PeriodOrderDate();
 
 
         }
@@ -48,6 +50,7 @@ namespace MySqLite
             {
                 try
                 {
+                    db.Open();
                     var rows = db.Query<Country>("select * from country join city on country.id = city.country_id");
                     foreach (var row in rows)
                     {
@@ -70,6 +73,7 @@ namespace MySqLite
             {
                 try
                 {
+                    db.Open();
                     var countries = db.Query<Country>("SELECT * FROM country").ToList();
                     var cities = db.Query<City>("SELECT * FROM city").ToList();
 
@@ -101,18 +105,19 @@ namespace MySqLite
             {
                 try
                 {
+                    db.Open();
                     var countries = db.Query<Country>("SELECT * FROM country").ToList();
                     var cities = db.Query<City>("SELECT * FROM city").ToList();
 
                     var groupedData = countries
                         .GroupJoin(
                             cities,
-                            c => c.id,      
-                            ci => ci.country_id, 
+                            c => c.id,
+                            ci => ci.country_id,
                             (c, cityGroup) => new
                             {
                                 Country = c.name,
-                                CityCount = cityGroup.Count() 
+                                CityCount = cityGroup.Count()
                             })
                         .OrderByDescending(x => x.CityCount);
 
@@ -137,6 +142,7 @@ namespace MySqLite
             {
                 try
                 {
+                    db.Open();
                     var countries = db.Query<Country>("SELECT * FROM country").ToList();
                     var cities = db.Query<City>("SELECT * FROM city").ToList();
 
@@ -171,6 +177,70 @@ namespace MySqLite
                     throw;
                 }
                 finally { db.Close(); }
+            }
+        }
+
+        public static void PeriodOrderDate()
+        {
+            using (var db = new SqliteConnection(@"Data Source=C:\Users\kimle\OneDrive\Документы\SQLite\MyDataBase.db"))
+            {
+                try
+                {
+                    db.Open();
+
+                    var products = db.Query<Product>("SELECT * FROM Product").ToList();
+                    var orders = db.Query<Order>("SELECT * FROM [Order]").ToList();
+                    var orderDetails = db.Query<OrderDetails>("SELECT * FROM OrderDetails").ToList();
+
+                    var res = orders
+                        .GroupJoin(
+                            orderDetails,
+                            o => o.order_id,
+                            od => od.order_id,
+                            (o, orderDetailGroup) => new
+                            {
+                                Order = o,
+                                OrderDetails = orderDetailGroup.ToList()
+                            })
+                        .Select(orderGroup => new
+                        {
+                            OrderId = orderGroup.Order.order_id,
+                            OrderDate = orderGroup.Order.order_date, 
+                            ParsedDate = DateTime.TryParse(orderGroup.Order.order_date, out var parsedDate) ? parsedDate : (DateTime?)null,
+                            ProductCount = orderGroup.OrderDetails.Count(),
+                            TotalQuantity = orderGroup.OrderDetails.Sum(od =>
+                                products.FirstOrDefault(p => p.product_id == od.product_id)?.quantity ?? 0),
+                            Products = orderGroup.OrderDetails.Select(od => new
+                            {
+                                ProductId = od.product_id,
+                                Quantity = products.FirstOrDefault(p => p.product_id == od.product_id)?.quantity ?? 0
+                            }).ToList()
+                        })
+                        .OrderByDescending(x => x.TotalQuantity)
+                        .Take(2)
+                        .ToList();
+
+                    foreach (var order in res)
+                    {
+                        Console.WriteLine($"Заказ #{order.OrderId} от {order.OrderDate}");
+                        Console.WriteLine($"Количество позиций: {order.ProductCount}");
+                        Console.WriteLine($"Общее количество товаров: {order.TotalQuantity}");
+                        Console.WriteLine("Продукты:");
+                        foreach (var product in order.Products)
+                        {
+                            Console.WriteLine($"- Product #{product.ProductId}: {product.Quantity} шт.");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+                finally
+                {
+                    db.Close();
+                }
             }
         }
 
